79367789a5f676458b1ec3fbbbb5b4fda84933c0
josh-data-eng <ihimirejosh@gmail.com>
2026-05-12 11:42:18 +0100

    added the rule to only sign-up when all fields are filled

--- signup.py
+++ signup.py
@@ -1,8 +1,8 @@
-name = (input("Enter Name :"))
-email = (input("Enter email :"))
-password = (input("Enter Password :"))
-user_details = (any([name, email, password]))
-if email.endswith("@gmail.com") and len(password) >= 7:
+name = input("Enter Name: ")
+email = input("Enter Email: ")
+password = input("Enter Password: ")
+fields_filled = all([name, email, password]) 
+if fields_filled and email.endswith("@gmail.com") and len(password) >= 7:
     print("Registration successful")
 else:
     print("Registration failed")
